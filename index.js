fe27f9d6f9aeb53a408730e9a693c3d88c72b9cc
Annie Eby <eby.annie@gmail.com>
2018-08-21 16:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // write your code below!
+function happyHolidays() {
+  console.log("Happy holidays!")
+}
+happyHolidays()
+function happyHolidays() {
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo(`you`)
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`)
+}
+happyHolidaysTo("you")
+
+function happyHolidaysTo(name) {
+  return(`Happy holidays ${name}!`)
+}
+happyHolidaysTo(`you`)
+
+function happyHolidaysTo(name) {
+  console.log(`Happy holidays, ${name}!`) 
+}
+happyHolidaysTo(`you`)
+
+function happyHolidayTo(holiday, name) {
+  console.log('Happy ${holiday}, ${name}!')
+}
+function happyHolidayTo(holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+happyHolidayTo(`Guy Fawkes Day`, `Man`)
+
+function holidayCountdown(holiday, days) {return (`It's ${days} days until ${holiday}!`)}
+holidayCountdown(20, `Mother's Day`)
